b7adf92dac5aa22bce98d03f1ab76fb40287b1ae
billofright <bill.yuan27@gmail.com>
2023-05-03 17:58:44 -0700

    comments table

--- database.sql
+++ database.sql
@@ -22,3 +22,12 @@ CREATE TABLE photos (
   post_id INTEGER NOT NULL,
   FOREIGN KEY (post_id) REFERENCES posts(id)
 );
+
+CREATE TABLE comments (
+  id SERIAL PRIMARY KEY,
+  content TEXT NOT NULL,
+  post_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+  FOREIGN KEY (post_id) REFERENCES posts(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
